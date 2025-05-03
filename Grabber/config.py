2555a37683ec6bf6dfc7f1656cf24212f8ce87f1
FLIRTERKUN <2005yashvashisht@gmail.com>
2025-05-03 22:45:47 +0530

    Update config.py

--- Grabber/config.py
+++ Grabber/config.py
@@ -1,15 +1,15 @@
 import os
 
-OWNER_ID = 1381668733
-GROUP_ID = "-1002225496870"
+OWNER_ID = 6265981509
+GROUP_ID = "-1002454723830"
 TOKEN = "7906639093:AAEQf5SKm6Q3ss_KHQdIU5tK4gmBTE3m_-c"
-api_id = 23734455
-api_hash = '40972650709e0e2b0aa58734f3524261'
+api_id = 21215287
+api_hash = 'c62635a8fff6247455494c1d8541215e'
 PHOTO_URL = ["https://files.catbox.moe/oai7m9.mp4"]
-SUPPORT_CHAT = "dragona_support"
-UPDATE_CHAT = "PickBotUpdatesHQ"
+SUPPORT_CHAT = "PHG_HEXA"
+UPDATE_CHAT = "PHG_HEXA"
 BOT_USERNAME = "Grab_athelete_robot"
 CHARA_CHANNEL_ID = -1002235251549
-MONGO_URL = "mongodb+srv://ishitaroy657boobs:vUKC7qfTpj0oTbii@cluster0.ct6shax.mongodb.net/"
+MONGO_URL = "mongodb+srv://yesvashisht2005:yash2005@cluster0.33ebnn3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME = "Character_catcher"
 LOG_CHAT_ID = -1002203193964
